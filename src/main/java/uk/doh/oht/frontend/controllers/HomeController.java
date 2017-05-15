e068187e222ea6419024341b89d6dbafa26b4a29
peter.whitehead <peter.whitehead@agilesphere.co.uk>
2017-05-15 15:59:01 +0100

    OHA:187 : Create a RINA client microservice

--- src/main/java/uk/doh/oht/frontend/controllers/HomeController.java
+++ src/main/java/uk/doh/oht/frontend/controllers/HomeController.java
@@ -7,17 +7,15 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by peterwhitehead on 28/04/2017.
  */
 @Controller
 @Slf4j
-@RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String displayRetrieveNextCase(final Model model) {
         log.info("Enter displayRetrieveNextCase");
         //add logged in users first name to model
